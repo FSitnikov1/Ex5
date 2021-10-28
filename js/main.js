472c2995bba638867a1b387a77328f8f2ea13b00
FSitnikov1 <mr.prizrek@mail.ru>
2021-10-28 17:48:54 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 function calculate() {
-    let type = document.getElementById("typ");
-    let cen;
-    switch (typ) {
-        case "1":
-            сen = 1199;
-            break;
-        case "2":
-            сen = 2799;
-            break;
+    var type, count, result;
+    type = document.getElementById('typ').value;
+    type = parseInt(type);
+
+    count = document.getElementById('count').value;
+    count = parseInt(count);
+    if (count == "") {
+        alert("This field is clear");
+    } else if ((count <= 0) || (count > 1999)) {
+        alert("Error of count!");
     }
-    let coun = document.getElementById("count");
-    if (coun == "") {
-        alert("Вы не указали количество");
-    }
-    else
-        if(coun > 999){
-            alert("Вы уверены?");
-        }
-        else {
-        let sum = сen * coun;
-        document.getElementById("sum").innerHTML = "Сумма равна: " + sum + " руб.";
+    if (type<0 || type>1000000) {
+        alert("Error");
+    } else {
+        result = type * count;
+        console.log("type");
+        console.log(type);
+        console.log("count");
+        console.log(count);
+        console.log("result");
+        console.log(result);
+        document.getElementById('sum').innerHTML = "Сумма равна: " + result + " руб.";
     }
 }
